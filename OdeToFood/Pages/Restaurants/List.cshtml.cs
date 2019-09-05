a686dab633689eb3d0102ef3780d870d00d16752
Arthur <arthur.github.37537@gmail.com>
2019-09-05 12:10:48 +0200

    Added Hello World message to reastaurant page

--- OdeToFood/Pages/Restaurants/List.cshtml.cs
+++ OdeToFood/Pages/Restaurants/List.cshtml.cs
@@ -7,10 +7,12 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace OdeToFood.Pages.Restaurants
 {
-    public class IndexModel : PageModel
+    public class ListModel : PageModel
     {
+        public string Message { get; set; }
         public void OnGet()
         {
+            Message = "Hello, World!";
         }
     }
 }
