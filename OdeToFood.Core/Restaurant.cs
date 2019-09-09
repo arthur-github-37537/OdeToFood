2fa8f9f723d0dd95815f906a7dbc629bb130881b
Arthur <arthur.github.37537@gmail.com>
2019-09-09 10:03:07 +0200

    View all restaurants

---
+++ OdeToFood.Core/Restaurant.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OdeToFood.Core
+{
+    public class Restaurant
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Location { get; set; }
+
+        public CuisineType Cuisine { get; set; }
+    }
+}
--- OdeToFood/Pages/Restaurants/List.cshtml.cs
+++ OdeToFood/Pages/Restaurants/List.cshtml.cs
@@ -4,15 +4,29 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Configuration;
+using OdeToFood.Data;
+using OdeToFood.Core;
 
 namespace OdeToFood.Pages.Restaurants
 {
     public class ListModel : PageModel
     {
+        private readonly IConfiguration config;
+        private readonly IRestaurantData restaurantData;
+
         public string Message { get; set; }
+        public IEnumerable<Restaurant> Restaurants { get; set; }
+
+        public ListModel(IConfiguration config, IRestaurantData restaurantData)
+        {
+            this.config = config;
+            this.restaurantData = restaurantData;
+        }
         public void OnGet()
         {
-            Message = "Hello, World!";
+            Message = config["Message"];
+            Restaurants = restaurantData.GetAll();
         }
     }
 }
