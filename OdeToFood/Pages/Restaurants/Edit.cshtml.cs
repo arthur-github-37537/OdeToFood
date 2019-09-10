1fce814190202ba8f8b7c803bcdef779935c6a9f
Arthur <arthur.github.37537@gmail.com>
2019-09-10 14:37:23 +0200

    Handling create vs update logic

--- OdeToFood/Pages/Restaurants/Edit.cshtml.cs
+++ OdeToFood/Pages/Restaurants/Edit.cshtml.cs
@@ -46,14 +46,31 @@ namespace OdeToFood.Pages.Restaurants
 
         public IActionResult OnPost()
         {
-            if(ModelState.IsValid)
+            if(!ModelState.IsValid)
             {
-                Restaurant = restaurantData.Update(Restaurant);
-                restaurantData.Commit();
-                return RedirectToPage("./Detail", new { restaurantId = Restaurant.Id });
+                Cuisines = htmlHelper.GetEnumSelectList<CuisineType>();
+                return Page();
             }
-            Cuisines = htmlHelper.GetEnumSelectList<CuisineType>();
-            return Page();
+
+            if (Restaurant.Id > 0)
+            {
+                restaurantData.Update(Restaurant);
+            }
+            else
+            {
+                restaurantData.Add(Restaurant);
+            }
+            restaurantData.Commit();
+            return RedirectToPage("./Detail", new { restaurantId = Restaurant.Id });
+
+            //if(ModelState.IsValid)
+            //{
+            //    Restaurant = restaurantData.Update(Restaurant);
+            //    restaurantData.Commit();
+            //    return RedirectToPage("./Detail", new { restaurantId = Restaurant.Id });
+            //}
+            //Cuisines = htmlHelper.GetEnumSelectList<CuisineType>();
+            //return Page();
         }
     }
 }
