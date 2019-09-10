74396c654c4a0015e4a10d243770ff563993f028
Arthur <arthur.github.37537@gmail.com>
2019-09-10 16:54:04 +0200

    Implemented entityframework dbcontext

---
+++ OdeToFood.Data/OdeToFoodDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using OdeToFood.Core;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OdeToFood.Data
+{
+    class OdeToFoodDbContext : DbContext
+    {
+        public DbSet<Restaurant> Restaurants { get; set; }
+    }
+}
