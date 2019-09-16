47004be6eaac9901b42800ca58b79718fcf46d06
Arthur <arthur.github.37537@gmail.com>
2019-09-16 11:09:07 +0200

    Added some comments to the delete pagemodel

--- OdeToFood/Pages/Restaurants/Delete.cshtml.cs
+++ OdeToFood/Pages/Restaurants/Delete.cshtml.cs
@@ -4,20 +4,25 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using OdeToFood.Core;
-using OdeToFood.Data;
+using OdeToFood.Core; // Contains Restaurant class
+using OdeToFood.Data; // Contains code for connection to database
 
 namespace OdeToFood.Pages.Restaurants
 {
     public class DeleteModel : PageModel
     {
+        // Restaurants contained in database
         private readonly IRestaurantData restaurantData;
+        // particular Restaurant
         public Restaurant Restaurant { get; set; }
 
+        // constructor gets all restaurants
         public DeleteModel(IRestaurantData restaurantData)
         {
             this.restaurantData = restaurantData;
         }
+
+        // Execute this code when the page loads
         public IActionResult OnGet(int restaurantId)
         {
             Restaurant = restaurantData.GetById(restaurantId);
@@ -28,6 +33,7 @@ namespace OdeToFood.Pages.Restaurants
             return Page();
         }
 
+        // Execute this code when the user confirms the delete
         public IActionResult OnPost(int restaurantId)
         {
             var restaurant = restaurantData.Delete(restaurantId);
